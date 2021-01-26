bafcf5e55a043340c358f69a95bf722aed1ed88b
Xsprone <60828917+Xsprone@users.noreply.github.com>
2021-01-26 13:09:15 -0300

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim intNum As Integer
+        intNum = 7
+        intNum = intNum + 1
+        MessageBox.Show("Valor: " & intNum.ToString, "Variaveis")
+    End Sub
+End Class
